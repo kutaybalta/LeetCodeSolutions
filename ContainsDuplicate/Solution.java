02cdc2ff2639589f21cf69146af3b046df57260a
Kutay Balta <kutaybalta@yahoo.com>
2023-07-13 10:13:21 +0300

    ContainsDuplicate

---
+++ ContainsDuplicate/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<>();
+        for (int num : nums) {
+            if (numbers.contains(num)) {
+                return true;
+            }
+            numbers.add(num);
+        }
+        return false;
+    }
+}
