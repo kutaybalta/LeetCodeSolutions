e8ff9eac5ecb9bdda1622ef9b73bfd183fce5678
Kutay Balta <kutaybalta@yahoo.com>
2023-07-13 10:21:44 +0300

    ContainsDuplicate

--- ContainsDuplicate/Solution.java
+++ ContainsDuplicate/Solution.java
@@ -5,10 +5,9 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> numbers = new HashSet<>();
         for (int num : nums) {
-            if (numbers.contains(num)) {
+            if (!numbers.add(num)) {
                 return true;
             }
-            numbers.add(num);
         }
         return false;
     }
